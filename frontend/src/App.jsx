e85790bbf229d801b18ea8384b3d4c4b08927548
falak <falakgala09@gmail.com>
2024-02-18 12:51:03 +0530

    feat:sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,37 @@
-export default function App() {
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import Sidebar from './components/Sidebar';
+
+// import { useAuthContext } from './context/AuthContext';
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import ExplorePage from './pages/ExplorePage';
+import LikesPage from './pages/LikesPage';
+
+const App = () => {
+  // const { authUser, loading } = useAuthContext();
+  // console.log('Authenticated user:', authUser);
+
+  // if (loading) return null;
+
   return (
-    <h1 className="text-3xl font-bold underline bg-red-400">Hello world!</h1>
+    <div className="flex">
+      <Sidebar />
+      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+        <Toaster />
+      </div>
+    </div>
   );
-}
+};
+
+export default App;
